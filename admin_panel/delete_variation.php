51140917863f35e136764f667079ba784fb9cfa8
Muhammad Asharif <asharifkhan245@gmail.com>
2025-08-26 14:49:12 +0500

    Updated code

--- admin_panel/delete_variation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once("connection.php");
-    $Id = $_GET["id"];
-    $delete = "DELETE FROM `variation_with_product` WHERE `var_id`= '$Id'";
-    mysqli_query($conn,$delete);
-    header("location:managevariations.php");
-
-?>
