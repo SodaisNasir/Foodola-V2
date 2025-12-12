20b9dca2e944c09284a673071fffd979f153c9c5
Muhammad Asharif <49174403+SodaisNasir@users.noreply.github.com>
2025-12-12 18:43:28 +0500

    New Inital code

--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,17 +2,16 @@
 
 
 // APP CONFIGRUATION
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
+$APP_NAME = "Kohinoor Indian";
+$BASE_URL = "https://kohinoorindian.foodola.shop/";
 $company_address = "Kuhngasse 1, 76684 Östringen";
 $company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
-
+$company_phone   = "0725326560-61";
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "bjwzeusrakgeawfn";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@kohinoorindian.de";
 
 
 
@@ -24,9 +23,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebndv4tpuahlbusgva3p6eut
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_kohinoorindian';
+$DB_USERNAME='u772850971_kohinoorindian';
+$DB_PASSWORD='2I=iOz;AYQG';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
