c254e3c9e7fc8dce0cedc9655cd0284a90a7da79
Muhammad Asharif <49174403+SodaisNasir@users.noreply.github.com>
2025-12-12 17:49:05 +0500

    New Initial code

--- API/connection.php
+++ API/connection.php
@@ -1,28 +1,28 @@
 <?php
 
 // APP CONFIGRUATION
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$FACEBOOK_URL = "https://facebook.com/foodola";
-$INSTAGRAM_URL  = "https://instagram.com/foodola";
-$TWITTER_URL  = "https://twitter.com/foodola";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$APP_NAME = "Burger Point Graben";
+$BASE_URL = "https://burgerpointgraben.de/";
+$FACEBOOK_URL = "https://facebook.com/burgerpointgraben";
+$INSTAGRAM_URL  = "https://instagram.com/burgerpointgraben";
+$TWITTER_URL  = "https://twitter.com/burgerpointgraben";
+$company_address = "Burger Point Heimservice";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 95975992";
 
 
 // PUSHER CONFIGRUATION
 $PUSHER_APP_KEY = "a1964c3ac950c1a0cdf5";
 $PUSHER_SECRET_KEY = "a711ec3a4b827eb6bcc5";
 $PUSHER_APP_ID ="1982652";
-$CHANNEL_1 = "orders";
-$CHANNEL_2 = "reservations";
+$CHANNEL_1 = "burgerpointgraben_orders";
+$CHANNEL_2 = "burgerpointgraben_reservations";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "bjwzeusrakgeawfn";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@burgerpointgraben.de";
 $ADMIN_EMAIL = "boundedsocial@gmail.com";
 
 
@@ -39,10 +39,9 @@ $LIEFERSOFT_COMPANY_ID = "228727d6-8910-4b85-8938-4010e7e4d4bd";
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
-
+$DB_DATABASE='u772850971_bp_graben';
+$DB_USERNAME='u772850971_bp_graben';
+$DB_PASSWORD='dYLdc@S3d8&Y';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,17 +2,17 @@
 
 
 // APP CONFIGRUATION
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$APP_NAME = "Burger Point Graben";
+$BASE_URL = "https://burgerpointgraben.de/";
+$company_address = "Burger Point Heimservice";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 95975992";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "bjwzeusrakgeawfn";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@burgerpointgraben.de";
 
 
 
@@ -24,9 +24,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebndv4tpuahlbusgva3p6eut
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_bp_graben';
+$DB_USERNAME='u772850971_bp_graben';
+$DB_PASSWORD='dYLdc@S3d8&Y';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
