a0f8b1325749d619f541a6ec29f82b7de3561f71
Muhammad Asharif <49174403+SodaisNasir@users.noreply.github.com>
2025-12-15 11:07:52 +0500

    New initial code

--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,18 +2,16 @@
 
 
 // APP CONFIGRUATION
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
-
+$APP_NAME = "Pizza Late Night";
+$BASE_URL = "https://pizzalatenight-ka.de/";
+$company_address = "Pizza Late Night";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 621122";
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "bjwzeusrakgeawfn";
-$FROM_EMAIL = "support@foodola.de";
-
+$FROM_EMAIL = "support@pizzalatenight.de";
 
 
 // ONESIGNAL CONFIGRUATION
@@ -24,9 +22,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebndv4tpuahlbusgva3p6eut
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_latenight';
+$DB_USERNAME='u772850971_latenight';
+$DB_PASSWORD='s!dJxdhy5A';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
