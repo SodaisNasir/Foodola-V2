39ed1ed8dad36e884f06d6e1eeb81ca3bd2b87b3
Muhammad Asharif <49174403+SodaisNasir@users.noreply.github.com>
2025-12-15 18:23:01 +0500

    New Initial code

--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,17 +2,17 @@
 
 
 // APP CONFIGRUATION
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$APP_NAME = "Himaliya Spicy";
+$BASE_URL = "https://himalayaspicy.foodola.shop/";
+$company_address = "Sophienstraße 59A, Karlsruhe";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 47042528";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "bjwzeusrakgeawfn";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@himalayaspicy.de";
 
 
 
@@ -24,9 +24,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebndv4tpuahlbusgva3p6eut
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_himalayaspicy';
+$DB_USERNAME='u772850971_himalayaspicy';
+$DB_PASSWORD='g1U;&1LaZd+';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
