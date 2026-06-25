87e5e7e4f4dce748af67271aa49e546b2e4555b6
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-06-25 15:41:21 +0500

    Version 4.0

--- API/connection.php
+++ API/connection.php
@@ -1,30 +1,29 @@
 <?php
 
 // APP CONFIGRUATION
-$LANG = 'en';
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$FACEBOOK_URL = "https://facebook.com/foodola";
-$INSTAGRAM_URL  = "https://instagram.com/foodola";
-$TWITTER_URL  = "https://twitter.com/foodola";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$LANG = 'de';
+$APP_NAME = "Meter Pizza";
+$BASE_URL = "https://meterpizzakarlsruhe.de/";
+$FACEBOOK_URL = "https://facebook.com/meterpizza";
+$INSTAGRAM_URL  = "https://instagram.com/meterpizza";
+$TWITTER_URL  = "https://twitter.com/meterpizza";
+$company_address = "Breite Straße 5476135 Karlsruhe";
+$company_city    = "Karlsruhe";
+$company_phone   = "080020207702";
 
 
 // PUSHER CONFIGRUATION
 $PUSHER_APP_KEY = "a1964c3ac950c1a0cdf5";
 $PUSHER_SECRET_KEY = "a711ec3a4b827eb6bcc5";
 $PUSHER_APP_ID ="1982652";
-$CHANNEL_1 = "orders";
-$CHANNEL_2 = "reservations";
+$CHANNEL_1 = "meterpizza_orders";
+$CHANNEL_2 = "meterpizza_reservations";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
-// $MAIL_PASSWORD = "crzeqyvqnffeuhzw";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@meterpizza.de";
 $ADMIN_EMAIL = "boundedsocial@gmail.com";
 
 
@@ -41,9 +40,9 @@ $LIEFERSOFT_COMPANY_ID = "228727d6-8910-4b85-8938-4010e7e4d4bd";
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_meterpizza';
+$DB_USERNAME='u772850971_meterpizza';
+$DB_PASSWORD='A4Z;oFD9p+f';
 
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,18 +2,18 @@
 
 
 // APP CONFIGRUATION
-$LANG = 'en';
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$LANG = 'de';
+$APP_NAME = "Meter Pizza";
+$BASE_URL = "https://meterpizzakarlsruhe.de/";
+$company_address = "Breite Straße 5476135 Karlsruhe";
+$company_city    = "Karlsruhe";
+$company_phone   = "080020207702";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@meterpizza.de";
 
 
 
@@ -25,9 +25,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebnchqsvtsgb4evjfpfzagsm
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_meterpizza';
+$DB_USERNAME='u772850971_meterpizza';
+$DB_PASSWORD='A4Z;oFD9p+f';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
