eac370fe7a5552473efcaf47feac0ab9d330fc53
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-07-31 18:41:31 +0500

    version 5.0

--- API/connection.php
+++ API/connection.php
@@ -1,31 +1,31 @@
 <?php
 
 // APP CONFIGRUATION
-$LANG = 'en';
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$FACEBOOK_URL = "https://facebook.com/foodola";
-$INSTAGRAM_URL  = "https://instagram.com/foodola";
-$TWITTER_URL  = "https://twitter.com/foodola";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$LANG = 'de';
+$APP_NAME = "Buffalo Burger & Pizza";
+$BASE_URL = "https://bufalloburgerbellheim.de/";
+$FACEBOOK_URL = "https://facebook.com/buffaloburgerpizza";
+$INSTAGRAM_URL  = "https://instagram.com/buffaloburgerpizza";
+$TWITTER_URL  = "https://twitter.com/buffaloburgerpizza";
+$company_address = "Hauptstraße 193, 76756 Bellheim";
+$company_city    = "Bellheim";
+$company_phone   = "07272 9334149";
 
 
 // PUSHER CONFIGRUATION
 $PUSHER_APP_KEY = "a1964c3ac950c1a0cdf5";
 $PUSHER_SECRET_KEY = "a711ec3a4b827eb6bcc5";
 $PUSHER_APP_ID ="1982652";
-$CHANNEL_1 = "orders";
-$CHANNEL_2 = "reservations";
+$CHANNEL_1 = "buffaloburgerpizza_orders";
+$CHANNEL_2 = "buffaloburgerpizza_reservations";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
 // $MAIL_PASSWORD = "crzeqyvqnffeuhzw";
-$FROM_EMAIL = "support@foodola.de";
-$ADMIN_EMAIL = "boundedsocial@gmail.com";
+$FROM_EMAIL = "support@buffaloburgerpizza.de";
+$ADMIN_EMAIL = "luv79783@gmail.com";
 
 
 // ONESIGNAL CONFIGRUATION
@@ -42,9 +42,9 @@ $LIEFERSOFT_COMPANY_ID = "228727d6-8910-4b85-8938-4010e7e4d4bd";
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_buffaloburger';
+$DB_USERNAME='u772850971_buffaloburger';
+$DB_PASSWORD='Ndwx142@';
 
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
