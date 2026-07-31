bc3caa358a1461e379f4bc783a6cd158f213b2c9
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-07-31 15:07:46 +0500

    version 5.0

--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -2,18 +2,18 @@
 
 
 // APP CONFIGRUATION
-$LANG = 'en';
-$APP_NAME = "Foodola";
-$BASE_URL = "https://foodola.foodola.shop/";
-$company_address = "Kuhngasse 1, 76684 Östringen";
-$company_city    = "Östringen";
-$company_phone   = "07253 26560-61";
+$LANG = 'de';
+$APP_NAME = "Pizza Sofort";
+$BASE_URL = "https://pizzasofort-ka.de/";
+$company_address = "Breitestr. 58, 76135 Karlsruhe";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 8408840";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@pizzasofort.de";
 
 
 
@@ -25,9 +25,9 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebnchqsvtsgb4evjfpfzagsm
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE='u772850971_foodola';
-$DB_USERNAME='u772850971_foodola';
-$DB_PASSWORD=']f0Hy^78Uf0s';
+$DB_DATABASE='u772850971_pizzasofort';
+$DB_USERNAME='u772850971_pizzasofort';
+$DB_PASSWORD='8[Y:/V=j?Yv';
 
 $conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
