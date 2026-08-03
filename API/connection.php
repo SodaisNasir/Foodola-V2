10faf25e28e5cc9054031132504d5cb3e286e02d
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-08-03 15:25:31 +0500

    version 5.0

--- API/connection.php
+++ API/connection.php
@@ -2,29 +2,29 @@
 
 // APP CONFIGRUATION
 $LANG = 'de';
-$APP_NAME = "Chick Pom";
-$BASE_URL = "https://chickpom.de/";
-$FACEBOOK_URL = "https://facebook.com/chickpom";
-$INSTAGRAM_URL  = "https://instagram.com/chickpom";
-$TWITTER_URL  = "https://twitter.com/chickpom";
-$company_address = "Hauptstraße 76, 76669 Bad Schönborn";
-$company_city    = "Bad Schönborn";
-$company_phone   = "07253 880905";
+$APP_NAME = "Pizza Late Night";
+$BASE_URL = "https://pizzalatenight-ka.de/";
+$FACEBOOK_URL = "https://facebook.com/latenight";
+$INSTAGRAM_URL  = "https://instagram.com/latenight";
+$TWITTER_URL  = "https://twitter.com/latenight";
+$company_address = "Pizza Late Night";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 621122";
 
 
 // PUSHER CONFIGRUATION
 $PUSHER_APP_KEY = "a1964c3ac950c1a0cdf5";
 $PUSHER_SECRET_KEY = "a711ec3a4b827eb6bcc5";
-$PUSHER_APP_ID = "1982652";
-$CHANNEL_1 = "chickpom_orders";
-$CHANNEL_2 = "chickpom_reservations";
+$PUSHER_APP_ID ="1982652";
+$CHANNEL_1 = "latenight_orders";
+$CHANNEL_2 = "latenight_reservations";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
 // $MAIL_PASSWORD = "crzeqyvqnffeuhzw";
-$FROM_EMAIL = "support@chickpom.de";
+$FROM_EMAIL = "support@pizzalatenight.de";
 $ADMIN_EMAIL = "boundedsocial@gmail.com";
 
 
@@ -42,14 +42,16 @@ $LIEFERSOFT_COMPANY_ID = "228727d6-8910-4b85-8938-4010e7e4d4bd";
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE = 'u772850971_chickpom';
-$DB_USERNAME = 'u772850971_chickpom';
-$DB_PASSWORD = '3cj&#kP^cO6:';
+$DB_DATABASE='u772850971_latenight';
+$DB_USERNAME='u772850971_latenight';
+$DB_PASSWORD='s!dJxdhy5A';
 
 
-$conn = mysqli_connect($Host, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+$conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+?>
--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -3,17 +3,17 @@
 
 // APP CONFIGRUATION
 $LANG = 'de';
-$APP_NAME = "Chick Pom";
-$BASE_URL = "https://chickpom.de/";
-$company_address = "Hauptstraße 76, 76669 Bad Schönborn";
-$company_city    = "Bad Schönborn";
-$company_phone   = "07253 880905";
+$APP_NAME = "Pizza Late Night";
+$BASE_URL = "https://pizzalatenight-ka.de/";
+$company_address = "Pizza Late Night";
+$company_city    = "Karlsruhe";
+$company_phone   = "0721 621122";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
-$FROM_EMAIL = "support@chickpom.de";
+$FROM_EMAIL = "support@pizzalatenight.de";
 
 
 
@@ -25,11 +25,11 @@ $ONE_SIGNAL_AUTH_KEY = "os_v2_app_asdjgef7prhj3hwj7l2yvlebnchqsvtsgb4evjfpfzagsm
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE = 'u772850971_chickpom';
-$DB_USERNAME = 'u772850971_chickpom';
-$DB_PASSWORD = '3cj&#kP^cO6:';
+$DB_DATABASE='u772850971_latenight';
+$DB_USERNAME='u772850971_latenight';
+$DB_PASSWORD='s!dJxdhy5A';
 
-$conn = mysqli_connect($Host, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+$conn = mysqli_connect($Host, $DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
 
 
 
@@ -40,3 +40,4 @@ $conn = mysqli_connect($Host, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+?>
