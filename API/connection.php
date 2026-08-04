4426a3bc434f5a74172e6cf2c36232c3b56e5fc0
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-08-04 12:30:14 +0500

    version 5.0

--- API/connection.php
+++ API/connection.php
@@ -2,29 +2,29 @@
 
 // APP CONFIGRUATION
 $LANG = 'de';
-$APP_NAME = "Chick Pom";
-$BASE_URL = "https://chickpom.de/";
-$FACEBOOK_URL = "https://facebook.com/chickpom";
-$INSTAGRAM_URL  = "https://instagram.com/chickpom";
-$TWITTER_URL  = "https://twitter.com/chickpom";
-$company_address = "Hauptstraße 76, 76669 Bad Schönborn";
-$company_city    = "Bad Schönborn";
-$company_phone   = "07253 880905";
+$APP_NAME = "Burger Place";
+$BASE_URL = "https://burgerplace-ger.de/";
+$FACEBOOK_URL = "https://facebook.com/burgerplaceger";
+$INSTAGRAM_URL  = "https://instagram.com/burgerplaceger";
+$TWITTER_URL  = "https://twitter.com/burgerplaceger";
+$company_address = "An der Hochschule 13 A76726 Germersheim";
+$company_city    = "Germersheim";
+$company_phone   = "07274 7799540";
 
 
 // PUSHER CONFIGRUATION
 $PUSHER_APP_KEY = "a1964c3ac950c1a0cdf5";
 $PUSHER_SECRET_KEY = "a711ec3a4b827eb6bcc5";
 $PUSHER_APP_ID = "1982652";
-$CHANNEL_1 = "chickpom_orders";
-$CHANNEL_2 = "chickpom_reservations";
+$CHANNEL_1 = "burgerplace_orders";
+$CHANNEL_2 = "burgerplace_reservations";
 
 
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "elliakydnklqykpv";
 // $MAIL_PASSWORD = "crzeqyvqnffeuhzw";
-$FROM_EMAIL = "support@chickpom.de";
+$FROM_EMAIL = "support@burgerplace-ger.de";
 $ADMIN_EMAIL = "boundedsocial@gmail.com";
 
 
@@ -42,9 +42,9 @@ $LIEFERSOFT_COMPANY_ID = "228727d6-8910-4b85-8938-4010e7e4d4bd";
 
 // DATABASE CONFIGRUATION
 $Host = 'localhost';
-$DB_DATABASE = 'u772850971_chickpom';
-$DB_USERNAME = 'u772850971_chickpom';
-$DB_PASSWORD = '3cj&#kP^cO6:';
+$DB_DATABASE='u772850971_burgerplace';
+$DB_USERNAME='u772850971_burgerplace';
+$DB_PASSWORD='qV4+tQf>hf91';
 
 
 $conn = mysqli_connect($Host, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
