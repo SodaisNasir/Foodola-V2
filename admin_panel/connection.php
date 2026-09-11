b50cad45dd788edf1d8d15686a5e278446efeb78
asharifkhan245 <130467544+asharifkhan245@users.noreply.github.com>
2026-09-11 16:30:50 +0500

    fix:credentials

--- admin_panel/connection.php
+++ admin_panel/connection.php
@@ -13,7 +13,7 @@ $company_phone   = "080020207702";
 // MAIL CONFIGRUATION
 $MAIL_USERNAME = "boundedsocial@gmail.com";
 $MAIL_PASSWORD = "csptxytwkopsdeht";
-$FROM_EMAIL = "support@foodola.de";
+$FROM_EMAIL = "support@superpizza.de";
 
 
 
